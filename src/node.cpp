6aa6deff9c8e13634cbfba01c3ee3c18ac0c8e1f
frederico-klein <frekle@kth.se>
2024-04-21 12:58:08 +0200

    added dummy publisher and a service thing that i don't know if it works or not

--- src/node.cpp
+++ src/node.cpp
@@ -1,10 +1,17 @@
 #include "ros/init.h"
 #include "ros/node_handle.h"
 #include "ros/publisher.h"
+#include "ros/rate.h"
 #include "ros/spinner.h"
+#include "ros/time.h"
+#include "sensor_msgs/Imu.h"
+#include "std_srvs/Empty.h"
+#include "std_srvs/EmptyRequest.h"
+#include "std_srvs/EmptyResponse.h"
 #include "ximu3_ros/Connection_ros.h"
 #include "ros/ros.h"
 #include "std_msgs/Float32.h"
+#include <functional>
 
 int get_divisor(std::string param_name,ros::NodeHandle nh)
 {
@@ -12,7 +19,7 @@ int get_divisor(std::string param_name,ros::NodeHandle nh)
 	if (nh.getParam(param_name + "_divisor", divisor_rate))
 	{
 		if (divisor_rate > 0)
-		{		
+		{
 			ROS_INFO_STREAM("Setting" << param_name << " divisor rate to:"<< divisor_rate);
 			ROS_INFO_STREAM("Will publish "<< param_name <<" at rate of: " << 400.0/divisor_rate);
 		}
@@ -27,7 +34,7 @@ int get_divisor(std::string param_name,ros::NodeHandle nh)
 		ROS_WARN_STREAM("Using default "<< param_name << " divisor rate: " << divisor_rate );
 	}
 
-	return divisor_rate; 
+	return divisor_rate;
 }
 
 int main(int argc, char** argv)
@@ -94,7 +101,7 @@ int main(int argc, char** argv)
 		ROS_WARN("Using default send_port: %d", send_port );
 		//ROS_WARN("Make sure port is unique and exposed in docker (both dockerfile and command line call!)");
 	}
-	
+
 	int ahrs_divisor_rate = get_divisor("ahrs", nh);
 	int sensors_divisor_rate = get_divisor("sensor", nh);
 
@@ -113,13 +120,59 @@ int main(int argc, char** argv)
 		bat_v_pub = nh.advertise<std_msgs::Float32>("battery/voltage",1);
 	}
 	imu_pub = nh.advertise<sensor_msgs::Imu>("imu",1);
-	
+
 	bool do_calib = false;
 	nh.getParam("do_calibration", do_calib);
 
-	Connection c( parent_frame_id, own_tf_name, ahrs_divisor_rate, sensors_divisor_rate, origin, temp_pub, bat_pub, bat_v_pub, imu_pub, publish_status, nh, do_calib);
-	//c.run(ximu3::UdpConnectionInfo("192.168.1.1", 9000, 8001));	
-	c.run(ximu3::UdpConnectionInfo(ip_address, receive_port, send_port));	
+	bool wait_to_start = false;
+	nh.getParam("wait_to_start", wait_to_start);
+
+
+	if (wait_to_start)
+	{
+		ros::Rate wait_rate(0.1);
+		bool start_now = false;
+		boost::function<bool(std_srvs::EmptyRequest& req, std_srvs::EmptyResponse& res)> wait_callback = [&start_now](auto req, auto res) -> bool
+		{
+			start_now = true;
+			return true;
+		};
+		//create a service, wait for that service call to change state so we can start
+		//
+		nh.advertiseService("start_now", wait_callback);
+
+		ROS_WARN("not_tested");
+		while (!start_now)
+		{
+			ROS_INFO_STREAM_THROTTLE(1,"Waiting to start");
+			wait_rate.sleep();
+			ros::spinOnce();
+		}
+	}
+
+
+	bool dummy_publisher=true;
+	nh.getParam("dummy_publisher", dummy_publisher);
+	if (dummy_publisher)
+	{
+		ROS_WARN_STREAM("Running dummy_publisher!!! Not attempting to connect to imu: "<<own_tf_name);
+		ros::Rate rr(400.0/ahrs_divisor_rate);
+
+		while(ros::ok())
+		{
+			sensor_msgs::Imu imu_msg;
+			imu_msg.header.stamp = ros::Time::now();
+			imu_pub.publish(imu_msg);
+			rr.sleep();
+			ros::spinOnce();
+		}
+	}
+	else
+	{
+		Connection c( parent_frame_id, own_tf_name, ahrs_divisor_rate, sensors_divisor_rate, origin, temp_pub, bat_pub, bat_v_pub, imu_pub, publish_status, nh, do_calib);
+		//c.run(ximu3::UdpConnectionInfo("192.168.1.1", 9000, 8001));
+		c.run(ximu3::UdpConnectionInfo(ip_address, receive_port, send_port));
+	}
 	return 0;
 }
 
