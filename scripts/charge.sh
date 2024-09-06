aa63b785c6797b67abba39897cbf5fd2ac86af71
frederico-klein <frekle@kth.se>
2024-09-06 09:33:39 +0200

    wait to start is a default now, probably it shouldn't

--- scripts/charge.sh
+++ scripts/charge.sh
@@ -3,7 +3,7 @@
 rosrun ximu3_ros temperature_graph.sh &
 rosrun ximu3_ros battery_charge_graph.sh &
 rosrun ximu3_ros battery_voltage_graph.sh &
-roslaunch ximu3_ros ximu_lower.launch ahrs_divisor:=0
+roslaunch ximu3_ros ximu_lower.launch ahrs_divisor:=0 wait_to_start:=false
 #rqt_plot 	/ximu_talus_l/temperature/data \
 #		/ximu_talus_r/temperature/data \
 #		/ximu_tibia_l/temperature/data \
